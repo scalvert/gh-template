a5e0958a81879e93d2d0ce1c56427ca7fbd69881
Steve Calvert <scalvert@linkedin.com>
2023-04-12 09:32:18 -0700

    Adding basic template functionality

--- go.mod
+++ go.mod
@@ -8,22 +8,31 @@ require (
 	github.com/AlecAivazis/survey/v2 v2.3.6 // indirect
 	github.com/alessio/shellescape v1.4.1 // indirect
 	github.com/briandowns/spinner v1.18.1 // indirect
+	github.com/cilium/ebpf v0.10.0 // indirect
 	github.com/cli/browser v1.1.0 // indirect
 	github.com/cli/cli/v2 v2.27.0 // indirect
 	github.com/cli/safeexec v1.0.1 // indirect
 	github.com/cli/shurcooL-graphql v0.0.2 // indirect
+	github.com/cosiner/argv v0.1.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/danieljoos/wincred v1.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/derekparker/trie v0.0.0-20221221181808-1424fce0c981 // indirect
 	github.com/fatih/color v1.7.0 // indirect
+	github.com/go-delve/delve v1.20.2 // indirect
+	github.com/go-delve/liner v1.2.3-0.20220127212407-d32d89dd2a5d // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/google/go-dap v0.8.0 // indirect
 	github.com/google/go-github/v39 v39.2.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/henvic/httpretty v0.0.6 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/itchyny/gojq v0.12.8 // indirect
 	github.com/itchyny/timefmt-go v0.1.3 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -33,19 +42,24 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.12.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rivo/uniseg v0.4.2 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shurcooL/githubv4 v0.0.0-20220520033151-0b4e3294ff00 // indirect
 	github.com/shurcooL/graphql v0.0.0-20220606043923-3cf50f8a0a29 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/testify v1.7.5 // indirect
 	github.com/thlib/go-timezone-local v0.0.0-20210907160436-ef149e42d28e // indirect
 	github.com/zalando/go-keyring v0.2.2 // indirect
+	go.starlark.net v0.0.0-20230302034142-4b1e35fe2254 // indirect
+	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/term v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,33 +1,55 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/user"
 	"path/filepath"
 
+	"github.com/cli/cli/v2/git"
 	"github.com/cli/cli/v2/pkg/cmdutil"
 	"github.com/spf13/cobra"
 )
 
 const CACHE_DIR string = ".pages"
 
+type TemplateOptions struct {
+	template string
+	outDir   string
+}
+
 func main() {
-	cmd := &cobra.Command{
+	templateOpts := &TemplateOptions{}
+
+	templateCmd := &cobra.Command{
 		Use:   "template <template name>",
 		Short: "Add a file template",
 		Args:  cmdutil.ExactArgs(1, "Missing argument for template"),
-		RunE:  runTemplate,
+		PreRun: func(cmd *cobra.Command, args []string) {
+			cwd, _ := os.Getwd()
+
+			if templateOpts.outDir == "." {
+				templateOpts.outDir = cwd
+			} else {
+				templateOpts.outDir = filepath.Join(cwd, templateOpts.outDir)
+			}
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			templateOpts.template = args[0]
+
+			return templateRun(templateOpts)
+		},
 	}
 
-	cmd.Flags().StringP("out-dir", "o", ".", "Output directory path")
+	templateCmd.Flags().StringVarP(&templateOpts.outDir, "out-dir", "o", ".", "Output directory path")
 
 	rootCmd := &cobra.Command{
 		Use:   "pages",
 		Short: "GitHub Pages commands",
 	}
 
-	rootCmd.AddCommand(cmd)
+	rootCmd.AddCommand(templateCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
@@ -62,19 +84,69 @@ func ensureCacheDir() string {
 	return cacheDir
 }
 
-func runTemplate(cmd *cobra.Command, args []string) error {
-	// ctx := context.Background()
+func getTemplateDirPath(cacheDir string) string {
+	return filepath.Join(cacheDir, "README-Template")
+}
 
-	// // Retrieve the template name argument.
-	// templateName := args[0]
+func cloneRepo(ctx context.Context, repoURL string, clonePath string) {
+	client := &git.Client{}
+	fmt.Println(clonePath)
+	target, err := client.Clone(ctx, repoURL, []string{clonePath})
 
-	// // Retrieve the output directory path option.
-	// outDir, _ := cmd.Flags().GetString("out-dir")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(target)
+}
+
+func ensureTemplateCache(ctx context.Context, cacheDir string, templateName string) {
+	templateDir := getTemplateDirPath(cacheDir)
+
+	_, err := os.Stat(templateDir)
+
+	if os.IsNotExist(err) {
+		cloneRepo(ctx, "https://linkedin.ghe.com/managed/README-Template", templateDir)
+	}
+}
+
+func readTemplate(templateName string) (string, error) {
+	templatePath := filepath.Join(getTemplateDirPath(ensureCacheDir()), templateName)
+	content, err := os.ReadFile(templatePath)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(content), nil
+
+}
+
+func writeTemplate(outDir string, templateName string, templateContent string) (string, error) {
+	templatePath := filepath.Join(outDir, templateName)
+
+	err := os.WriteFile(templatePath, []byte(templateContent), 0644)
+
+	return templatePath, err
+}
+
+func templateRun(templateOpts *TemplateOptions) error {
+	ctx := context.Background()
 
 	cacheDir := ensureCacheDir()
 
-	fmt.Println(cacheDir)
+	ensureTemplateCache(ctx, cacheDir, templateOpts.template)
 
+	template, err := readTemplate(templateOpts.template)
+
+	if err != nil {
+		fmt.Println("No template found with the template name:", templateOpts.template)
+		return nil
+	}
+
+	templatePath, _ := writeTemplate(templateOpts.outDir, templateOpts.template, template)
+
+	fmt.Println("Template written to:", templatePath)
 	// // Decode the template file content.
 	// templateContent, err := content.Decode()
 	// if err != nil {
